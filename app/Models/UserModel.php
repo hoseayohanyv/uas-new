4d2e2fc8906cc1fd856bd04420075c88d62f1e03
hoseayohanyv <80023498+hoseayohanyv@users.noreply.github.com>
2025-07-17 20:27:18 +0700

    Update UserModel.php

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,7 +5,8 @@ use CodeIgniter\Model;
 
 class UserModel extends Model
 {
-    protected $table = 'users';
+    protected $table      = 'users';
     protected $primaryKey = 'id';
     protected $allowedFields = ['username', 'email', 'password', 'role'];
+    protected $useTimestamps = true;
 }
